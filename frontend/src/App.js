ba8ea1400561ae6bcfc749425c721ed366250ece
Ndeheu <adonisndeheu@gmail.com>
2023-03-21 10:38:17 +0100

    Create-React-App-01

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import './index.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">Amazon</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
